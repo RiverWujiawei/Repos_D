7007342b9d4427077d05cb5aef61771a022cf7cb
RiverWujiawei <RiverWujiawei>
2022-11-18 10:09:12 +0000

    test4
    test4

--- Sample/Sample_notebook.py
+++ Sample/Sample_notebook.py
@@ -1,3 +1,10 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC select * from hive_metastore.default.nyc_taxi limit 3;
+
+# COMMAND ----------
+
+name = "tomy"
+print (f"hello {name}")
+%run ./Sample_notebook01
+print (f"Welcome back {fullname}")
---
+++ Sample/Sample_notebook01.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+fullname = "joe"
+print(fullname)
